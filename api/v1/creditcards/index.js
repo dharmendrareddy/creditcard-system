155338dd3cda3f78f0725b2e2bd0abdb954078a9
Dharma <dharmendrareddylakkireddy@Dharmendras-MacBook-Pro.local>
2022-06-25 23:56:54 +0530

    Logger class added

--- api/v1/creditcards/index.js
+++ api/v1/creditcards/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const redis = require('../../../lib/services/redis');
+const Logger = require('../../../lib/Logger');
+const log = new Logger('CreditCard');
 
 const getCards = async (req, resp) => {
     const data = await redis.get('cards');
     const cards = JSON.parse(data);
+    log.info('CreditCard', 'cards fetched successfully !!');
     return resp.send(cards);
 };
 
@@ -14,6 +17,7 @@ const addCard = async (req, resp) => {
     const cards = JSON.parse(existedCards) || [];
     cards.push(card);
     await redis.set('cards', JSON.stringify(cards));
+    log.info('CreditCard', 'card successfully added !!');
     return resp.send('card successfully added !!');
 };
 
