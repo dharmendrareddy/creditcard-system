8398bf2211c8c5c2845adf9063ea405056acc50f
Dharma <dharmendrareddylakkireddy@Dharmendras-MacBook-Pro.local>
2022-06-28 00:23:28 +0530

    test case adjusted

--- test/unit/creditCards.unit.test.js
+++ test/unit/creditCards.unit.test.js
@@ -1,12 +1,14 @@
 const chance = require('chance').Chance();
 const { saveCard, fetchCards } = require('../../lib/services/creditCard');
+const { isValidNumber } = require('../../lib/utils');
 
 beforeAll(async () => { });
 
 it('Should create a new credit card and save in DB', async () => {
+  const cardNumber = chance.integer({ min: 8, length: 16 });
   const fakeCreditCard = {
     name: chance.name({ length: 80 }),
-    cardNumber: chance.integer({ min: 8, length:16 }),
+    cardNumber: isValidNumber(cardNumber) ? cardNumber : 4444333322221111,
     balance: 0,
     limit: chance.integer({ min: 0, max: 1000000 })
   };
@@ -19,4 +21,4 @@ it('Should fetch existed creds from DB', async () => {
   expect(cards).toHaveLength(cards.length);
 });
 
-afterAll(async () => {});
+afterAll(async () => { });
